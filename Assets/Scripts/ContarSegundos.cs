6da321fe3c64718aaf61b2e33bab0cb8c10dd63f
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-05-26 15:54:23 -0300

    Initial commit

---
+++ Assets/Scripts/ContarSegundos.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContarSegundos : MonoBehaviour
+{
+    public float timeElapsed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeElapsed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            timeElapsed += Time.deltaTime;
+        }
+
+        Debug.Log(Mathf.Floor(timeElapsed));
+
+        //float segundos = Mathf.Floor(Time.time);
+        //Debug.Log(segundos);
+
+    }
+}
---
+++ Assets/Scripts/TimeCounter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour
+{
+    public Text txtTimeCounter;
+    float elapsedTime;
+    bool isPaused;
+
+    float timeInstantiate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = 0;
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused == false)
+        {
+            elapsedTime += Time.deltaTime;
+
+            if (elapsedTime % timeInstantiate)
+            {
+
+            }
+        }
+
+        txtTimeCounter.text = Mathf.Floor(elapsedTime).ToString();
+    }
+}
